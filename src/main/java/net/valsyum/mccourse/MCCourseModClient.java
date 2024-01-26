07a73c4bee86f83b63d9d39fd89842ddf46a2f2c
BuildTools <unconfigured@null.spigotmc.org>
2024-01-26 04:10:55 +0200

    Initial commit

---
+++ src/main/java/net/valsyum/mccourse/MCCourseModClient.java
@@ -0,0 +1,10 @@
+package net.valsyum.mccourse;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MCCourseModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
